87b02c36fe65f804f77475f49f4b36bac0ea470a
arjuuuuunnnnn <arjunbanur27@gmail.com>
2025-04-18 22:12:58 +0530

    edit: git utils and its test file

--- zor/git_utils.py
+++ zor/git_utils.py
@@ -4,9 +4,12 @@ import typer
 def git_commit(message: str):
     """Create a git commit with the given message"""
     try:
-        if subprocess.run(["git", "add", "."], check=True).returncode != 0:
+        try:
+            subprocess.run(["git", "add", "."], check=True)
+        except subprocess.CalledProcessError:
             subprocess.run(["git", "init"], check=True)
-        subprocess.run(["git", "add", "."], check=True)
+            subprocess.run(["git", "add", "."], check=True)
+
         subprocess.run(["git", "commit", "-m", message], check=True)
         return True
     except Exception as e:
